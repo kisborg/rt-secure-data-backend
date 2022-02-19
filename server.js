8cbf2f1660419c3d866e097998c84f138f676fde
kisborg <mark.borgstrom@gmail.com>
2022-02-19 09:16:08 +0100

    added cors

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+  },
+});
 
 const port = process.env.PORT || 3000;
 
